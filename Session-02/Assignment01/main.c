066212fed34ec2edd29a48c14e5a8ca7f6ed17f0
Ahmed-Ayman-khedr <ahmedelbanhawy2001@gmail.com>
2024-02-14 21:48:39 +0200

    Percentage second way

--- Session-02/Assignment01/main.c
+++ Session-02/Assignment01/main.c
@@ -13,5 +13,6 @@ int main (){
     mu=number_1%number_2;
     printf("%d + %d = %d .\n%d - %d = %d .\n",number_1,number_2,pl,number_1,number_2,mi);
     printf("%d ",number_1);printf("%c",ch);printf(" %d = %d .\n",number_2,mu);
+    printf("%d %% %d = %d .\n",number_1,number_2,mu);
     return 0;
 }
