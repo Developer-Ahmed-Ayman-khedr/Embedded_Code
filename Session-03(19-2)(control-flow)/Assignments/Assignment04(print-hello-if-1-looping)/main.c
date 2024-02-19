e6f925bc575a20bb79d614d3d573274bccd8e27b
Ahmed-Ayman-khedr <ahmedelbanhawy2001@gmail.com>
2024-02-19 21:08:22 +0200

    While loop and its assignment.

---
+++ Session-03(19-2)(control-flow)/Assignments/Assignment04(print-hello-if-1-looping)/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int number=1;
+    while(number==1){
+        printf(" Hello:\nIf you want to repeat please enter 1:\n");
+        scanf("%d",&number);
+    }
+    return 0;
+}
