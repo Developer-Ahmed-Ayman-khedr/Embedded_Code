d2b1d624a5abe8468b5da39ec7d6925c41b027c7
Ahmed-Ayman-khedr <ahmedelbanhawy2001@gmail.com>
2024-02-19 21:48:57 +0200

    Upload the do-while loop assignment

---
+++ Session-03(19-2)(control-flow)/Assignments/Assignment04(print-hello-looping-for-number-enter-by-user)/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int number;
+    printf("Please enter number of hellos to print:\n");
+    scanf("%d",&number);
+    do
+    {
+        printf("Hello.\n");
+        number--;
+    }
+    while(number>=1);
+    printf("Final.\n");
+    return 0;
+}
