98bdd4052c29eaa50419b85bce87fc477f3885cb
Ahmed-Ayman-khedr <ahmedelbanhawy2001@gmail.com>
2024-02-19 18:56:35 +0200

    update.

--- Session-03(19-2)(control-flow)/if-condition)/main.c
+++ Session-03(19-2)(control-flow)/if-condition)/main.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-    int number = 1 ;
+    int number = 6 ;
     if ( number > 2 )
     {
         printf("Yes\n");
+    }
+    else
+    {
+        printf("No.\n");
     }
     printf("cont.\n");
     return 0;
