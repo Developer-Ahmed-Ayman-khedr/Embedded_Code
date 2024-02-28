a45e76a3b7cda08dc35e6546de17fc53802b2aa4
Ahmed-Ayman-khedr <ahmedelbanhawy2001@gmail.com>
2024-02-28 18:31:00 +0200

    Update the first task (addition in a call by refference function) and uplaod the second task (addition in a call by refference void function)

---
+++ Session-06(28-2)()/Tasks/02-(addition-in-a-call-by-refference-void-function)/func.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void addition(int* numb_1, int* numb_2, int* numb_3){
+    *numb_3=*numb_1+*numb_2;
+}
---
+++ Session-06(28-2)()/Tasks/02-(addition-in-a-call-by-refference-void-function)/func.h
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include "func.c"
+
+void addition(int* numb_1, int* numb_2, int* numb_3);
