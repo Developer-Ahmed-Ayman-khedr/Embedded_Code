ce7af05f16748f904bd6f6d09443b676698e9849
Ahmed-Ayman-khedr <ahmedelbanhawy2001@gmail.com>
2024-03-04 17:05:06 +0200

    Update Assignment 3 (increment value of number by 1 inside a function)(ASG_4-1)

--- Session-05(26-2)(pointers)/Assignments/Assignment03-(increment-value-of-number-by-1-inside-a-function)(ASG_4-1)/func.c
+++ Session-05(26-2)(pointers)/Assignments/Assignment03-(increment-value-of-number-by-1-inside-a-function)(ASG_4-1)/func.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void edit(int* numb){
-    ++*numb;
+void edit(int numb[]){
+    numb[0]++;
+    numb[1]=5;
+    numb[2]=20;
+    numb[3]=80;
 }
--- Session-05(26-2)(pointers)/Assignments/Assignment03-(increment-value-of-number-by-1-inside-a-function)(ASG_4-1)/main.c
+++ Session-05(26-2)(pointers)/Assignments/Assignment03-(increment-value-of-number-by-1-inside-a-function)(ASG_4-1)/main.c
@@ -3,11 +3,14 @@
 #include "func.h"
 
 int main(){
-    int number;
+    int number[4];
     printf("Enter the value of x\n");
-    scanf("%d",&number);
-    printf("Before calling edit x = %d\n",number);
-    edit(&number);
-    printf("After calling edit x = %d\n",number);
+    scanf("%d",&number[0]);
+    printf("Before calling edit x = %d\n",number[0]);
+    edit(number);
+    printf("After calling edit x = %d\n",number[0]);
+    printf("%d\n",number[1]);
+    printf("%d\n",number[2]);
+    printf("%d\n",number[3]);
     return 0;
 }
