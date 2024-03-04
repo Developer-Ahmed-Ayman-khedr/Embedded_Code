6e63e48223246d3951ddb7c681fa4dca5d2cbd29
Ahmed-Ayman-khedr <ahmedelbanhawy2001@gmail.com>
2024-03-04 19:53:48 +0200

    Upload Example 5 (scan elements in array)

---
+++ Session-07(4-3)(array)/Examples/example05-(scan-elements-in-array)/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "func.h"
+
+int main(){
+    int arr[4] = {10,20,30,40};
+    for (int i = 0; i < 4; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%d\n",arr[i]);
+    }
+    return 0;
+}
