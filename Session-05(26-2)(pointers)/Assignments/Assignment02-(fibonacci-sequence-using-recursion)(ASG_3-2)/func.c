c05570c39d4b94c4e32b0d18dd3103febd3446f3
Ahmed-Ayman-khedr <ahmedelbanhawy2001@gmail.com>
2024-03-04 17:48:33 +0200

    Update Assignment 2 (fibonacci sequence using recursion)(ASG_3-2))

--- Session-05(26-2)(pointers)/Assignments/Assignment02-(fibonacci-sequence-using-recursion)(ASG_3-2)/func.c
+++ Session-05(26-2)(pointers)/Assignments/Assignment02-(fibonacci-sequence-using-recursion)(ASG_3-2)/func.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int numb_1=0,numb_2=1,numb_3;
 
 void fibonacci(int numb){
+    static int numb_1=0,numb_2=1,numb_3;
     if(numb>0)
     {
         numb_3 = numb_1 + numb_2;    
--- Session-05(26-2)(pointers)/Assignments/Assignment02-(fibonacci-sequence-using-recursion)(ASG_3-2)/main.c
+++ Session-05(26-2)(pointers)/Assignments/Assignment02-(fibonacci-sequence-using-recursion)(ASG_3-2)/main.c
@@ -6,18 +6,25 @@ int main(){
     int number_of_terms;
     printf("Enter the number of terms\n");
     scanf("%d",&number_of_terms);
-    if (number_of_terms==1)
+    if (number_of_terms<=0)
     {
-        printf("Fibonacci Series: 0");
-    }
-    else if (number_of_terms==2)
-    {
-        printf("Fibonacci Series: 0, 1");
+        printf("Error.\n");
     }
     else
     {
+        if (number_of_terms==1)
+        {
+        printf("Fibonacci Series: 0");
+        }
+        else if (number_of_terms==2)
+        {
+        printf("Fibonacci Series: 0, 1");
+        }
+        else
+        {
         printf("Fibonacci Series: 0, 1, ");
         fibonacci(number_of_terms-2);
+        }
     }
     return 0;
 }
