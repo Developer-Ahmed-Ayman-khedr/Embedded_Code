cf7ead78333974e0db2aa504ce53c4033d5628db
Ahmed-Ayman-khedr <ahmedelbanhawy2001@gmail.com>
2024-03-11 21:23:29 +0200

    Update Task 3 (count the repetition of a number)

--- Session-08(6-3)(array-extended)/Tasks/task03-(count-the-repetition-of-a-number)/main.c
+++ Session-08(6-3)(array-extended)/Tasks/task03-(count-the-repetition-of-a-number)/main.c
@@ -3,8 +3,15 @@
 #include "func.h"
 
 int main(){
-    int coun = 0, arr[5] = {1,3,4,3,5};
-    fun(arr,3,&coun);
+    int coun = 0, arr[5], i, number_to_search;
+    for ( i = 0; i < 5; i++)
+    {
+        printf("Enter number:\n");
+        scanf("%d",arr+i);
+    }
+    printf("Enter number to search for:\n");
+    scanf("%d",&number_to_search);
+    fun(arr,number_to_search,&coun);
     printf("repetation = %d",coun);
     return 0;
 }
