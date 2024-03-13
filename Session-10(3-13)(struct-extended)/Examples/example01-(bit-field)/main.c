9f94c696fbdca1bca2f4f987c4678b8c2b46465f
Ahmed-Ayman-khedr <ahmedelbanhawy2001@gmail.com>
2024-03-13 21:04:46 +0200

    Update Example 1 (bit field)

--- Session-10(3-13)(struct-extended)/Examples/example01-(bit-field)/main.c
+++ Session-10(3-13)(struct-extended)/Examples/example01-(bit-field)/main.c
@@ -4,11 +4,14 @@
 
 struct BIT{
     int num_1 : 3;
-    int num_1 : 2;
-    int num_1 : 4;
+    int num_2 : 2;
+    int num_3 : 4;
 };
 
 int main(){
-    
+    struct BIT b1 = {7,1,2};
+    printf("%d\n",b1.num_1);
+    printf("%d\n",b1.num_2);
+    printf("%d\n",b1.num_3);
     return 0;
 }
