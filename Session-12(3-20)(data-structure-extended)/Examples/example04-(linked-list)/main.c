09ce0f7859dbcdabcf8d70099d208e0c6a334d58
Ahmed-Ayman-khedr <ahmedelbanhawy2001@gmail.com>
2024-03-20 22:09:23 +0200

    Update

--- Session-12(3-20)(data-structure-extended)/Examples/example04-(linked-list)/main.c
+++ Session-12(3-20)(data-structure-extended)/Examples/example04-(linked-list)/main.c
@@ -8,6 +8,16 @@ typedef struct Node{
 }node;
 
 int main(){
-    node* ptr=(node*)malloc(1*sizeof(node));
+    node* one=(node*)malloc(1*sizeof(node));
+    node* two=(node*)malloc(1*sizeof(node));
+    node* three=(node*)malloc(1*sizeof(node));
+    one->value = 10;
+    one->next = two;
+    two->value = 20;
+    two->next = three;
+    three->value = 30;
+    printf("%d\n",one->value);
+    printf("%d\n",one->next->value);
+    printf("%d\n",one->next->next->value);
     return 0;
 }
