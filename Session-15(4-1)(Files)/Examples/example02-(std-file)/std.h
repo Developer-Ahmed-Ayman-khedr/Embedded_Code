d627e55d6afdb470e14af5429671216b433a1802
Ahmed Khedr <94747792+Developer-Ahmed-Khedr@users.noreply.github.com>
2024-04-01 23:32:42 +0200

    Update example 2 (std file).

--- Session-15(4-1)(Files)/Examples/example02-(std-file)/std.h
+++ Session-15(4-1)(Files)/Examples/example02-(std-file)/std.h
@@ -1,10 +1,10 @@
 #ifdef STD_H
 #define STD_H
-typedef unsigned char u8
-typedef signed char s8
-typedef unsigned short u16
-typedef signed short s16
-typedef unsigned int u32
-typedef signed int s32
-typedef enum {false, true} bool
+typedef unsigned char u8;
+typedef signed char s8;
+typedef unsigned short u16;
+typedef signed short s16;
+typedef unsigned int u32;
+typedef signed int s32;
+typedef enum {false, true} bool;
 #endif 
