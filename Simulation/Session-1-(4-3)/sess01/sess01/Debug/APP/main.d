085ebdba45bffea9c1c7fe54aa64a61161a3b5d3
Ahmed Khedr <94747792+Developer-Ahmed-Khedr@users.noreply.github.com>
2024-04-03 23:46:18 +0200

    .

--- Simulation/Session-1-(4-3)/sess01/sess01/Debug/APP/main.d
+++ Simulation/Session-1-(4-3)/sess01/sess01/Debug/APP/main.d
@@ -1,4 +1,4 @@
-main.d main.o: .././main.c ../LIB/BIT_MATH.h ../LIB/STD_TYPES.h \
+APP/main.d APP/main.o: ../APP/main.c ../LIB/BIT_MATH.h ../LIB/STD_TYPES.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
