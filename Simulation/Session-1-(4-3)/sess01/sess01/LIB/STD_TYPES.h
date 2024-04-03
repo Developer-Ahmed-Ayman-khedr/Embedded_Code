8fbaeab42ad297a1f46918a99d1a64f4dd135312
Ahmed Khedr <94747792+Developer-Ahmed-Khedr@users.noreply.github.com>
2024-04-03 23:01:33 +0200

    Upload.

---
+++ Simulation/Session-1-(4-3)/sess01/sess01/LIB/STD_TYPES.h
@@ -0,0 +1,22 @@
+/*
+ * STD_TYPES.h
+ *
+ * Created: 4/3/2024 8:58:56 PM
+ *  Author: ahmed
+ */ 
+
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+typedef unsigned char u8;
+typedef signed char s8;
+typedef unsigned short u16;
+typedef signed short s16;
+typedef unsigned int u32;
+typedef signed int s32;
+typedef enum {false, true} bool;
+
+
+
+#endif /* STD_TYPES_H_ */
---
+++ Simulation/Session-1-(4-3)/sess01/sess01/main.c
@@ -0,0 +1,25 @@
+/*
+ * sess01.c
+ *
+ * Created: 4/3/2024 8:50:30 PM
+ * Author : ahmed
+ */ 
+#include "BIT_MATH.h"
+#include "STD_TYPES.h"
+#define F_CPU 1600000UL
+#include<util/delay.h>
+
+int main(void)
+{
+	u8* ptrDDRC = (u8*)0x34;
+	u8* ptrPORTC = (u8*)0x35;
+	SET_BIT(*ptrDDRC,7);
+    while (1) 
+    {
+		SET_BIT(*ptrPORTC,7);
+		_delay_ms(500);
+		CLEAR_BIT(*ptrPORTC,7);
+		_delay_ms(500);
+    }
+}
+
