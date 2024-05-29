b58f1f7b01bc068e2fe511ef40091d50da0ce6e4
Ahmed Khedr <94747792+Developer-Ahmed-Ayman-khedr@users.noreply.github.com>
2024-05-29 22:53:50 +0300

    Upload EEPROM driver and its implementation and EEPROMPASS Task.

---
+++ Microchip-code/code/APP/Sess29EEPROM/TASKEEPROMPASS/TASKEEPROMPASS_INT.h
@@ -0,0 +1,63 @@
+/*
+ * TASKEEPROMPASS_INT.h
+ *
+ * Created: 5/29/2024 9:38:28 PM
+ *  Author: ahmed
+ */ 
+
+
+#ifndef TASKEEPROMPASS_INT_H_
+#define TASKEEPROMPASS_INT_H_
+
+
+/******************************************
+INCLUDES
+*******************************************/
+
+#include "DIO_INT.h"
+
+#include "LCD_INT.h"
+
+#include "KPD_INT.h"
+
+#include "ADC_INT.h"
+
+#include "GI_INT.h"
+
+#include "TIMER0_INT.h"
+
+#include "TIMER1_INT.h"
+
+#include "WDT_INT.h"
+
+#include "UART_INT.h"
+
+#include "SPI_INT.h"
+
+#include "I2C_INT.h"
+
+#include "EEPROM_INT.h"
+
+/******************************************
+Global Data TYPES AND STRUCTURES
+*******************************************/
+
+
+/******************************************
+GLOBAL CONSTANT MACROS
+*******************************************/
+
+
+/******************************************
+GLOBAL FUNCTIONS MACROS
+*******************************************/
+
+
+/******************************************
+GLOBAL FUNCTIONS Prototypes
+*******************************************/
+
+int source_code();
+
+
+#endif /* TASKEEPROMPASS_INT_H_ */
